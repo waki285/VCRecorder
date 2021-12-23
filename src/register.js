e58d50cbac61554ba673d6aebb1471a7ea86289f
waki285 <suzuneu@suzuneu.com>
2021-12-23 00:29:11 +0000

    ✨ feat(SlashCommand): add register

---
+++ src/register.js
@@ -0,0 +1,23 @@
+const { Client, ClientApplication } = require("discord.js");
+
+const commands = [
+  {
+    name: "start",
+    description: "録音を開始します。"
+  },
+  {
+    name: "stop",
+    description: "録音を停止し、ファイルを出力します。"
+  }
+];
+
+(async () => {
+  const client = new Client({ intents: 0 });
+  client.token = process.env.BOT_TOKEN;
+  client.application = new ClientApplication(client, {});
+  await client.application.fetch();
+
+  await client.application.commands.set(commands, process.argv[2]);
+
+  console.log("Registration Success.");
+})()
